a098ded54cffa5ce774d547dc50e7bd1b3269a6a
shrutiverma05 <shrutive01@gmail.com>
2023-01-19 23:19:37 +0530

    haystack edit

--- haystackv2/Haystack.py
+++ haystackv2/Haystack.py
@@ -12,10 +12,12 @@ hostedserverUrl = "https://novacorpweb.azurewebsites.net/"
 
 def download(data_file,index_name):
     url = hostedserverUrl+index_name+'/'+data_file
+    print(url)
     urllib.request.urlretrieve(url, data_file)
     
 def train(data_file,index_name,retriever,document_store):
     download(data_file,index_name)
+    print('download done')
     df = pd.read_csv(data_file)
     df.fillna(value="", inplace=True)
     df["question"] = df["question"].apply(lambda x: x.strip())
@@ -23,16 +25,20 @@ def train(data_file,index_name,retriever,document_store):
     # Get embeddings for our questions from the FAQs
 
     questions = list(df["question"].values)
+    print('before try')
     try:
         df["question_emb"] = retriever.embed_queries(queries=questions).tolist()
     except:
         df["question_emb"] = retriever.embed_queries(texts=questions)
     df = df.rename(columns={"question": "content"})
+    print('after try')
 
     # Convert Dataframe to list of dicts and index them in our DocumentStore
 
     docs_to_index = df.to_dict(orient="records")
+    print('second last')
     document_store.write_documents(docs_to_index)
+    print('last')
 
 app = FastAPI()
 
@@ -55,6 +61,7 @@ async def index(index_name, background_tasks: BackgroundTasks):
         use_gpu=True,
         scale_score=False,
     ))
+    print('document store and retriever initialized')
     background_tasks.add_task(train, data_file, index_name, retriever, document_store)
     return f"Training started for {index_name}"
 
